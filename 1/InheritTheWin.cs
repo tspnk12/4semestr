83745a2d02fa2fe759e6163756767a6eb2465906
tspnk12 <46029856+tspnk12@users.noreply.github.com>
2020-06-22 23:26:52 +0500

    Add files via upload

---
+++ 1/InheritTheWin.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Petzold.InheritTheWin
+{
+    class InheritTheWin : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new InheritTheWin());
+        }
+        public InheritTheWin()
+        {
+            Title = "Inherit the Win";
+        }
+    }
+}
