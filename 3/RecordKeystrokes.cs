0929c38d91585468ee533c950bd979eec3597d44
tspnk12 <46029856+tspnk12@users.noreply.github.com>
2020-06-22 23:28:34 +0500

    Add files via upload

---
+++ 3/RecordKeystrokes.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Petzold.RecordKeystrokes
+{
+    public class RecordKeystrokes : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new RecordKeystrokes());
+        }
+        public RecordKeystrokes()
+        {
+            Title = "Record Keystrokes";
+            Content = "";
+        }
+        protected override void OnTextInput(TextCompositionEventArgs args)
+        {
+            base.OnTextInput(args);
+            string str = Content as string;
+
+            if (args.Text == "\b")
+            {
+                if (str.Length > 0)
+                    str = str.Substring(0, str.Length - 1);
+            }
+            else
+            {
+                str += args.Text;
+            }
+            Content = str;
+        }
+    }
+}
